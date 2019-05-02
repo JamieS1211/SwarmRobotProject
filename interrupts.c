c2ff406cb5dffdedf68c9e4f09aaddd6379bc08e
JamieS1211 <jamieshepherd1211@gmail.com>
2019-05-02 12:38:27 +0100

    Improved comments

--- interrupts.c
+++ interrupts.c
@@ -103,9 +103,9 @@ void __interrupt() interrupts_Event(void) {
         uint16_t value = vl5310x_ReadRange(0x52);
         
         
-        uint8_t data[1];
-        data[0] = (uint8_t)value;
-        mRF89XA_DataSend(data, 1);  
+        //uint8_t data[1];
+        //data[0] = (uint8_t)value;
+        //mRF89XA_DataSend(data, 1);  
         
         if (value > 350) { //Go straight
             enableLeft = 1;
--- mRF89XA.c
+++ mRF89XA.c
@@ -42,7 +42,8 @@ void mRF89XA_Setup(void) {
     mRF89XA_Configuration();
 }
 
-void mRF89XA_Configuration(void) {//General Configuration Registers
+void mRF89XA_Configuration(void) {
+    //General Configuration Registers
     mRF89XA_ConfigWrite(0x01, 0x8C); //DMODREG: DATA AND MODULATION CONFIGURATION REGISTER p31
     mRF89XA_ConfigWrite(0x02, 0x0A); //FDEVREG: FREQUENCY DEVIATION CONTROL REGISTER p32
     mRF89XA_ConfigWrite(0x03, 0x07); //BRSREG: BIT RATE SET REGISTER p32
@@ -56,35 +57,35 @@ void mRF89XA_Configuration(void) {//General Configuration Registers
     //mRF89XA_ConfigWrite(0x0B, 0x32); //S2CREG: S2 COUNTER SET REGISTER  p36 - unused as RPS = 0 in GCONREG
     //mRF89XA_ConfigWrite(0x0C, 0x38); //PACREG: POWER AMPLIFIER CONTROL REGISTER p37 - unused as RPS = 0 in GCONREG
 
-     //Interrupt Configuration Registers
+    //Interrupt Configuration Registers
     mRF89XA_ConfigWrite(0x0D, 0x18); //FTXRXIREG: FIFO TRANSMIT AND RECEIVE INTERRUPT REQUEST CONFIGURATION REGISTER p38
     mRF89XA_ConfigWrite(0x0E, 0x3F); //FTPRIREG: FIFO TRANSMIT PLL AND RSSI INTERRUPT REQUEST CONFIGURATION REGISTER p40
     mRF89XA_ConfigWrite(0x0F, 0x00); //RSTHIREG: RSSI THRESHOLD INTERRUPT REQUEST CONFIGURATION REGISTER p41
 
 
-     //Receiver Configuration Registers
+    //Receiver Configuration Registers
     mRF89XA_ConfigWrite(0x10, 0xA3); //FILCREG: FILTER CONFIGURATION REGISTER p42
     mRF89XA_ConfigWrite(0x11, 0x38); //PFCREG: POLYPHASE FILTER CONFIGURATION REGISTER p43
     mRF89XA_ConfigWrite(0x12, 0x38); //SYNCREG: SYNC CONTROL REGISTER p44
     mRF89XA_ConfigWrite(0x15, 0x00); //OOKCREG: OOK CONFIGURATION REGISTER p46
 
 
-     //Sync Word Configuration Registers
+    //Sync Word Configuration Registers
     mRF89XA_ConfigWrite(0x16, 0x21); //SYNCV31REG:SYNCVALUEFIRSTBYTECONFIGURATIONREGISTER p47
     mRF89XA_ConfigWrite(0x17, 0x22); //SYNCV23REG:SYNCVALUESECONDBYTECONFIGURATIONREGISTER p47
     mRF89XA_ConfigWrite(0x18, 0x23); //SYNCV15REG:SYNCVALUETHIRDBYTECONFIGURATIONREGISTER p48
     mRF89XA_ConfigWrite(0x19, 0x24); //SYNCV07REG:SYNCVALUEFOURTHBYTECONFIGURATIONREGISTER p48
 
 
-     //Transmitter Configuration Registers
+    //Transmitter Configuration Registers
     mRF89XA_ConfigWrite(0x1A, 0x70); //TXCONREG: TRANSMIT PARAMETER CONFIGURATION REGISTER p49
 
 
-     //Oscillator Configuration Registers
+    //Oscillator Configuration Registers
     mRF89XA_ConfigWrite(0x1B, 0x3C); //CLKOUTREG: CLOCK OUTPUT CONTROL REGISTER p50
 
 
-     //Packet Handling Configuration Registers
+    //Packet Handling Configuration Registers
     mRF89XA_ConfigWrite(0x1C, 0x01); //PLOADREG: PAYLOAD CONFIGURATION REGISTER p51
     mRF89XA_ConfigWrite(0x1D, 0x00); //NADDSREG: NODE ADDRESS SET REGISTER p51
     mRF89XA_ConfigWrite(0x1E, 0x60); //PKTCREG: PACKET CONFIGURATION REGISTER p52
@@ -131,7 +132,7 @@ void mRF89XA_ConfigWrite(uint8_t registerAddress, uint8_t byte) {
     
     CSCON = 0;
     
-    // Bits 7, 6 and 0 represent start, read/write and stop respectively
+    // Bits 7, 6, 5:1 and 0 represent start, read/write, address and stop respectively
     addressByte |= (registerAddress << 1); // Append register address
     
     mRF89XA_DuplexTransmit(addressByte);
@@ -146,7 +147,7 @@ uint8_t mRF89XA_ConfigRead(uint8_t registerAddress) {
     
     CSCON = 0;
     
-    // Bits 7, 6 and 0 represent start, read/write and stop respectively
+    // Bits 7, 6, 5:1 and 0 represent start, read/write, address and stop respectively
     addressByte |= 0x40; // Append read/write bit to 1 (indicate read)
     addressByte |= (registerAddress << 1); // Append register address
     
@@ -161,9 +162,8 @@ uint8_t mRF89XA_ConfigRead(uint8_t registerAddress) {
 void mRF89XA_DataSend(uint8_t data[], uint8_t numberOfBytes) {
     
     uint8_t startValue = mRF89XA_ConfigRead(0x00);
-    bool modeChange = ((startValue & 0xE0) != 0x20);
     
-    if (modeChange) {
+    if ((startValue & 0xE0) != 0x20) {
         mRF89XA_ConfigWrite(0x00, (startValue & 0x1F) | 0x20); //Bit mask first 3 bits to 0s, then OR operator to change to '001' (standby mode)
     }
     
@@ -181,7 +181,7 @@ void mRF89XA_DataSend(uint8_t data[], uint8_t numberOfBytes) {
 
     while (IRQ1 == 0);
     
-    wait(32);
+    wait(50);
     
     mRF89XA_ConfigWrite(0x00, startValue); 
 }
